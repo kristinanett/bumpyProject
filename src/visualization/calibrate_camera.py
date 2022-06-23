9c25891acc0e1f261e33b463437f45e3409bf1a3
kristinanett <kristinanett@gmail.com>
2022-06-23 19:18:54 +0200

    Added camera calibration and path visualization base code

--- src/visualization/calibrate_camera.py
+++ src/visualization/calibrate_camera.py
@@ -7,50 +7,68 @@ import matplotlib.pyplot as plt
 """
 Implement the number of vertical and horizontal corners
 """
-nb_horizontal = 8
-nb_vertical = 5
+# nb_horizontal = 8
+# nb_vertical = 5
 
 
-# prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
-objp = np.zeros((nb_horizontal*nb_vertical,3), np.float32)
-objp[:,:2] = np.mgrid[0:nb_vertical,0:nb_horizontal].T.reshape(-1,2)
-objp = objp*33.6 #multiplying with the actual size of the checker board square
+# # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+# objp = np.zeros((nb_horizontal*nb_vertical,3), np.float32)
+# objp[:,:2] = np.mgrid[0:nb_vertical,0:nb_horizontal].T.reshape(-1,2)
+# objp = objp*3.3 #33.6 multiplying with the actual size of the checker board square
 
-# Arrays to store object points and image points from all the images.
-objpoints = [] 
-imgpoints = [] 
+# # Arrays to store object points and image points from all the images.
+# objpoints = [] 
+# imgpoints = [] 
 
-images = glob.glob('data/raw/calibration/frame*.png') #/home/kristin/bumpyProject/data/raw/calibration/frame001142.png
+images = sorted(glob.glob('data/raw/calibration/frame*.png')) #/home/kristin/bumpyProject/data/raw/calibration/frame001142.png
 
-assert images
-for fname in images:
-    img = cv2.imread(fname)
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# assert images
+# for fname in images:
+#     img = cv2.imread(fname)
+#     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    ret, corners = cv2.findChessboardCorners(gray, (nb_vertical, nb_horizontal), None)
+#     ret, corners = cv2.findChessboardCorners(gray, (nb_vertical, nb_horizontal), None)
 
-    # If found, add object points, image points (after refining them)
-    if ret == True:
-        objpoints.append(objp)
-        imgpoints.append(corners)
+#     # If found, add object points, image points (after refining them)
+#     if ret == True:
+#         objpoints.append(objp)
+#         imgpoints.append(corners)
 
-        # Draw and display the corners
-#         img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners,ret)
-#         cv2.imshow('img',img)
-#         cv2.waitKey(500)
+#         # Draw and display the corners
+# #         img = cv2.drawChessboardCorners(img, (nb_vertical,nb_horizontal), corners,ret)
+# #         cv2.imshow('img',img)
+# #         cv2.waitKey(500)
 
-# cv2.destroyAllWindows()
+# # cv2.destroyAllWindows()
 
-ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-# print(ret)
-print(mtx)
-# [[584.78019224   0.         740.35969583]
-#  [  0.         585.35875633 376.26496857]
-#  [  0.           0.           1.        ]]
-# print(dist)
-# print(rvecs)
-# print(tvecs)
+# #obtaining and then saving camera parameters to a file for later use
+# ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+# np.savez("calib_results.npz", ret=ret, mtx=mtx, dist=dist, rvecs=rvecs, tvecs=tvecs)
 
+# print(mtx)
+# # [[584.78017866   0.         740.35975462]
+# #  [  0.         585.3587445  376.26495791]
+# #  [  0.           0.           1.        ]]
+
+
+####################################################################################################
+#reading in the camera parameters from a file for testing
+npzfile = np.load("calib_results.npz")
+ret, mtx, dist, rvecs, tvecs = npzfile["ret"], npzfile["mtx"], npzfile["dist"], npzfile["rvecs"], npzfile["tvecs"]
+
+#reading a test image
 img = cv2.imread(images[0])
-h,  w = img.shape[:2]
+#img = cv2.imread("data/processed/imgs/frame000878.png")
+h,  w = img.shape[:2] #Original: 732 , 1490
 newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+# crop the image
+#x, y, w, h = roi #Undistorted: 1206 , 711
+#dst = dst[y:y+h, x:x+w]
+cv2.imshow('original', img)
+cv2.imshow('calibresult', dst)
+cv2.waitKey()
+#cv2.imwrite('calibresult.png', dst)
+cv2.destroyAllWindows()
---
+++ src/visualization/show_path.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2
+debug_dt = 0.25 #random number from the paper
+x = 0.38 #distance from front wheel to robot centre
+
+#function from paper
+def commands_to_positions(linvel, angvel):
+    N = len(linvel) #8
+    all_angles = [np.zeros(N)]
+    all_positions = [np.zeros((N, 2))] #8, 2
+    for linvel_i, angvel_i in zip(linvel.T, angvel.T):
+        angle_i = all_angles[-1] + debug_dt * angvel_i
+        position_i = all_positions[-1] + debug_dt * linvel_i[..., np.newaxis] * np.stack([np.cos(angle_i), np.sin(angle_i)], axis=1)
+
+        all_angles.append(angle_i)
+        all_positions.append(position_i)
+
+    all_positions = np.stack(all_positions, axis=1)
+    return all_positions
+
+#function from paper
+def project_points(xy):
+    """
+    :param xy: [batch_size, horizon, 2]
+    :return: [batch_size, horizon, 2]
+    """
+    batch_size, horizon, _ = xy.shape
+    npzfile = np.load("calib_results.npz")
+    ret, mtx, dist, rvecs, tvecs = npzfile["ret"], npzfile["mtx"], npzfile["dist"], npzfile["rvecs"], npzfile["tvecs"]
+
+    # camera is ~0.35m above ground
+    xyz = np.concatenate([xy, -0.35 * np.ones(list(xy.shape[:-1]) + [1])], axis=-1) # 0.35
+    rvec = tvec = (0, 0, 0)
+    camera_matrix = mtx
+
+    # x = y
+    # y = -z
+    # z = x
+    xyz[..., 0] += 0.15  # NOTE(greg): shift to be in front of image plane
+    xyz_cv = np.stack([xyz[..., 1], -xyz[..., 2], xyz[..., 0]], axis=-1)
+    uv, _ = cv2.projectPoints(xyz_cv.reshape(batch_size * horizon, 3), rvec, tvec, camera_matrix, dist)
+    uv = uv.reshape(batch_size, horizon, 2)
+
+    return uv
+
+
+linvel = np.array([[0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5, 0.5]])
+
+ang = np.array([[0.2, 0.2, 0.2, 0.3, 0.3, 0.2, 0.1, 0.1]])
+r = x/np.tan(ang) #turning radiuses for all the angles
+angvel = linvel/r #angular velocities
+
+pos = commands_to_positions(linvel, angvel)
+pixels = project_points(pos)
+print(pos)
+print(pixels)
+
+x_list, y_list = [], []
+img = cv2.imread("data/processed/imgs/frame000878.png")
+im_lims = img.shape
+for pix in pixels:
+    pix_lims = (732., 1490.) #(480., 640.)
+
+    assert pix_lims[1] / pix_lims[0] == im_lims[1] / float(im_lims[0])
+    resize = im_lims[0] / pix_lims[0]
+
+    pix = resize * pix
+    x_list.append(im_lims[1] - pix[:, 0])
+    y_list.append(im_lims[0] - pix[:, 1])
+
+print(x_list) #weird values (not on image)
+print(y_list) #weird values (not on image)
+
+img_draw = img.copy()
+
+for i in range(len(x_list)-1):
+    cv2.line(img_draw, (x_list[i], y_list[i]), (x_list[i+1], y_list[i+1]), (255, 0, 0), 2)
+
+cv2.imshow("Show path", img_draw)
+cv2.waitKey()
+cv2.destroyAllWindows()
