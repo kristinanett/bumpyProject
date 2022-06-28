d2199a6d8bb7a41beb158c8bde6ab2921b1377a0
kristinanett <kristinanett@gmail.com>
2022-06-28 17:26:18 +0200

    Made a plot of commands for the report

--- src/visualization/plot_commands.py
+++ src/visualization/plot_commands.py
@@ -5,7 +5,7 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser(description="Plot mqtt data from a file.")
-parser.add_argument("file", help="Input file.")
+parser.add_argument("file", help="Input file.") #for example "/data/raw/1506/mqtt/06-15-13-33/dir/log000"
 
 args = parser.parse_args()
 
@@ -14,19 +14,33 @@ f = open(args.file, "r")
  
 times = []
 zs = []
+xs = []
 
+first = True
 for line in f:
-    times.append(json.loads(line)["time"]/1000)
+    if first == True:
+        first_time = json.loads(line)["time"]/1000
+        first = False
+    times.append((json.loads(line)["time"]/1000)-first_time)
     zs.append(json.loads(line)["message"]["twist"]["angular"]["z"])
+    xs.append(json.loads(line)["message"]["twist"]["linear"]["x"])
 
 #plotting the data
-fig, ax = plt.subplots()
-ax.plot(zs, times)
-ax.set_xlim(0.5, -0.5)
-ax.set_xlabel('decreasing radians')
-ax.set_ylabel('time')
-ax.grid(True)
+fig, ax = plt.subplots(2, 1)
 
+ax[0].plot(times[:3000], xs[:3000])
+ax[0].set_ylim(0.45, 0.55)
+#ax[0].set_xlabel('time (s)', fontsize = 12)
+ax[0].set_ylabel('linear velocity (m/s)', fontsize = 12)
+ax[0].grid(True)
+
+ax[1].plot(times[:3000], zs[:3000])
+ax[1].set_ylim(-0.5, 0.5)
+ax[1].set_xlabel('time (s)', fontsize = 12)
+ax[1].set_ylabel('wheel angle (rad)', fontsize = 12)
+ax[1].grid(True)
+
+fig.tight_layout()
 plt.show()
 
 # Closing file
